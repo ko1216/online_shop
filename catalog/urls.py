308177b81a2fa596642a5924156c18fc7598e354
korchaginks <kkonstantinserg@gmail.com>
2023-09-04 23:13:38 +0300

    -исправлены юрлы для удобного вызова шаблонных тегов

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,7 +8,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', index, name='ko1216-shop'),
     path('contact/', contact, name='contact'),
-    path('catalog/', catalog, name='catalog'),
-    path('<int:pk>/catalog/categories/', catalog_category, name='catalog_category'),
-    path('<int:pk>/catalog/products/', product, name='product')
+    path('catalog/', catalog, name='all_products'),
+    path('categories/<int:pk>', catalog_category, name='catalog_category'),
+    path('product/<int:pk>', product, name='product')
 ]
