a9da8f60cdb891b73fe85232aff07691fb1e797a
korchaginks <kkonstantinserg@gmail.com>
2023-09-09 21:21:55 +0300

    -Переписаны контроллеры с FBV на CBV, переименованы шаблоны, исправлены переменные внутри шаблонов для работы с CBV, исправлены url path под актуальные контроллеры. Контроллер странички контактов не тронут, так как не совсем CRUD но при этом не подходит для TemplateView, если переписывать его на ListView, то строк кода становится больше

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contact, catalog, catalog_category, product
+from catalog.views import contact, IndexListView, CatalogListView, CategoryListView, ProductListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='ko1216-shop'),
+    path('', IndexListView.as_view(), name='ko1216-shop'),
     path('contact/', contact, name='contact'),
-    path('catalog/', catalog, name='all_products'),
-    path('categories/<int:pk>', catalog_category, name='catalog_category'),
-    path('product/<int:pk>', product, name='product')
+    path('catalog/', CatalogListView.as_view(), name='all_products'),
+    path('categories/<int:pk>', CategoryListView.as_view(), name='catalog_category'),
+    path('product/<int:pk>', ProductListView.as_view(), name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from catalog.models import Category, Product
 
 
-def index(request):
-    context = {
-        'object_list': Category.objects.all(),
-        'product_list': Product.objects.all(),
-        'title': 'Главная ko1216-shop'
-    }
-    return render(request, 'catalog/index.html', context)
+class IndexListView(ListView):
+    model = Category
+    template_name = 'catalog/index.html'
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexListView, self).get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
 
 
 def contact(request):
@@ -25,27 +28,37 @@ def contact(request):
     return render(request, 'catalog/contact.html', context)
 
 
-def catalog(request):
-    context = {
-        'product_list': Product.objects.all(),
-        'title': 'Каталог товаров'
-    }
-    return render(request, 'catalog/catalog.html', context)
+class CatalogListView(ListView):
+    model = Category
+    template_name = 'catalog/catalog.html'
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super(CatalogListView, self).get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
 
 
-def catalog_category(request, pk):
-    category_product = Category.objects.get(pk=pk)
-    context = {
-        'product_list': Product.objects.filter(category=pk),
-        'title': f'Категория {category_product.name}'
-    }
-    return render(request, 'catalog/categories.html', context)
+class CategoryListView(ListView):
+    model = Category
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['products'] = Product.objects.filter(pk=self.kwargs['pk'])
+        return context
 
 
-def product(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {
-        'product_list': Product.objects.filter(pk=pk),
-        'title': f'Товар {product.name}'
-    }
-    return render(request, 'catalog/product.html', context)
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(pk=self.kwargs['pk'])
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
