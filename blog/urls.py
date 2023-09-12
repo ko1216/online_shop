e4bcc4b90c60c0e01795292e2c23b14f25138fb9
korchaginks <kkonstantinserg@gmail.com>
2023-09-12 17:22:31 +0300

    -написаны юрлы для блога

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from blog.apps import BlogConfig
+from blog.views import BlogListView, BlogCreateView, BlogUpdateView, BlogDeleteView, BlogDetailView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('/', BlogListView.as_view(), name='blog_list'),
+    path('/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('/update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('/delete/<int:pk>', BlogDeleteView.as_view(), name='blog_delete')
+]
