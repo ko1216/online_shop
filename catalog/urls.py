917aa4ec238b572416a30c0b237b68270366c9c5
korchaginks <kkonstantinserg@gmail.com>
2023-09-22 23:33:28 +0300

    Написаны юрлы и контроллеры

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contact, IndexListView, CatalogListView, CategoryListView, ProductListView
+from catalog.views import contact, IndexListView, CatalogListView, CategoryListView, ProductListView, ProductCreateView, ProductUpdateView
 
 app_name = CatalogConfig.name
 
@@ -10,5 +10,7 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('catalog/', CatalogListView.as_view(), name='all_products'),
     path('categories/<int:pk>', CategoryListView.as_view(), name='catalog_category'),
-    path('product/<int:pk>', ProductListView.as_view(), name='product')
+    path('product/<int:pk>', ProductListView.as_view(), name='product'),
+    path('product/create', ProductCreateView.as_view(), name='create_product'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
 
-from catalog.models import Category, Product
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Category, Product, Version
 
 
 class IndexListView(ListView):
@@ -36,6 +39,7 @@ class CatalogListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(CatalogListView, self).get_context_data(**kwargs)
         context['products'] = Product.objects.all()
+        context['versions'] = Version.objects.all()
         return context
 
 
@@ -45,7 +49,7 @@ class CategoryListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(CategoryListView, self).get_context_data(**kwargs)
-        context['products'] = Product.objects.filter(pk=self.kwargs['pk'])
+        context['products'] = Product.objects.filter(category=self.kwargs['pk'])
         return context
 
 
@@ -62,3 +66,41 @@ class ProductListView(ListView):
         context = super(ProductListView, self).get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:all_products')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'catalog/product_update_form.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:all_products')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        context['categories'] = Category.objects.all()
+        context['formset'] = VersionFormset()
+        if self.request.method == 'POST':
+            context['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context['formset'] = VersionFormset(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+            
+        return super().form_valid(form)
