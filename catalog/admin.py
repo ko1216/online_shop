f813c4f45f6c18321a53b1534ed4361ccca03b55
korchaginks <kkonstantinserg@gmail.com>
2023-09-22 23:32:56 +0300

    Добавлена новая модель Написана админка под модель

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Version
 
 
 @admin.register(Category)
@@ -13,3 +13,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(Version)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'version', 'is_active',)
+    list_filter = ('is_active',)
+    search_fields = ('name', 'version',)
--- catalog/models.py
+++ catalog/models.py
@@ -32,3 +32,18 @@ class Product(models.Model):
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
         ordering = ('category', 'price',)
+
+
+class Version(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Версия')
+    version = models.CharField(max_length=40, verbose_name='Номер версии')
+    is_active = models.BooleanField(default=True, verbose_name='Активна')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
+        ordering = ('name', 'is_active',)
