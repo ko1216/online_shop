b9196b4473eeb865f9529a2068e5ca82c099d0f9
korchaginks <kkonstantinserg@gmail.com>
2023-10-01 19:01:34 +0300

    Изменены юрлы блога

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from blog.views import BlogListView, BlogCreateView, BlogUpdateView, BlogDeleteV
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('/', BlogListView.as_view(), name='blog_list'),
+    path('', BlogListView.as_view(), name='blog_list'),
     path('/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('/create/', BlogCreateView.as_view(), name='blog_create'),
     path('/update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
