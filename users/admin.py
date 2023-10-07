39ed98c7f54164d7e7756b06ab7234d835c727c4
korchaginks <kkonstantinserg@gmail.com>
2023-10-07 12:14:04 +0300

    Изменил отображение в админке модели User

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from users.models import User
 
 
-admin.site.register(User)
+# admin.site.register(User)
+@admin.register(User)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'email_is_verified')
