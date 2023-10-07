78a272af69c07b2cd2ad80e7460c75a1f8f383cf
korchaginks <kkonstantinserg@gmail.com>
2023-10-07 12:14:47 +0300

    Добавил в модель User поле, куда сохраняется token пользователя

--- users/models.py
+++ users/models.py
@@ -7,11 +7,13 @@ from catalog.models import NULLABLE
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Почта')
-    email_is_verified = models.BooleanField(default=False)
+    email_is_verified = models.BooleanField(default=False, verbose_name='Почта подтверждена')
 
     phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
+
+    token = models.TextField(verbose_name='Токен', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
